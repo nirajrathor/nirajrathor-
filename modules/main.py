21b680de975dcecf2a38f8c00beb7fea3dc900d6
Einsteinamarjeetk <156225038+Einsteinamarjeetk@users.noreply.github.com>
2024-05-12 21:47:40 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -33,6 +33,10 @@ bot = Client(
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("Hi!\n\nGive /txt Command to Downlaod From a Text file.\n")
 
+@bot.on_message(filters.command("stop"))
+async def restart_handler(_, m):
+    await m.reply_text("Process Has Been Stopped Successfully !", True)
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
 @bot.on_message(filters.command("restart"))
 async def restart_handler(_, m):
